15b4c16e4f62868083a85e8bd9a6231177b3f20f
Priyanshu <papai4517@gmail.com>
2024-12-23 21:11:58 +0530

    updated Admin.jsx

--- synclass/src/components/AdminPage.jsx
+++ synclass/src/components/AdminPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./../styles/AdminPage.css";
+import "./CreateTimetable";
+import fileImage from "../assets/file.png";
 
 const AdminPage = () => {
   const navigate = useNavigate();
@@ -10,11 +11,52 @@ const AdminPage = () => {
   };
 
   return (
-    <div className="admin-page">
-      <h1>Admin Dashboard</h1>
-      <button onClick={handleCreateTimetable}>Create Timetable</button>
+    <div
+      style={{
+        textAlign: "center",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#4DA1A9",
+        margin: 0,
+      }}
+    >
+      <img
+        src={fileImage}
+        alt="Timetable Icon"
+        style={{ maxWidth: "400px", maxHeight: "400px", marginBottom: "40px" }}
+      />
+
+      <button
+        style={{
+          background: "none",
+          color: "#79D7BE",
+          border: "2px solid",
+          padding: "1em 2em",
+          fontSize: "1em",
+          transition: "color 0.25s, border-color 0.25s, box-shadow 0.25s, transform 0.25s",
+        }}
+        onMouseOver={(e) => {
+          e.target.style.borderColor = "#F6F4F0";
+          e.target.style.color = "white";
+          e.target.style.boxShadow = "0 0.5em 0.5em -0.4em #F6F4F0";
+          e.target.style.transform = "translateY(-0.25em)";
+        }}
+        onMouseOut={(e) => {
+          e.target.style.borderColor = "#2E5077";
+          e.target.style.color = "#2E5077";
+          e.target.style.boxShadow = "none";
+          e.target.style.transform = "none";
+        }}
+        onClick={handleCreateTimetable}
+      >
+        Create Timetable
+      </button>
     </div>
   );
 };
 
 export default AdminPage;
+
